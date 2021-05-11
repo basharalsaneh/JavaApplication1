cbf17f7eec79bc1d6fb9a5d45562c3b6beac8f9f
basharalsaneh <83920082+basharalsaneh@users.noreply.github.com>
2021-05-11 10:33:38 +0200

    Initial commit

---
+++ src/javaApplication1/JavaApplication1.java
@@ -0,0 +1,23 @@
+package javaApplication1;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import oru.inf.InfDB;
+import oru.inf.InfException;
+
+public class JavaApplication1 {
+
+    private static InfDB idb;
+
+    public static void main(String[] args) throws InfException {
+
+        try {
+            idb = new InfDB("mibdb", "3306", "mibdba", "mibkey");
+
+        }   catch (InfException ex){
+            Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+
+}
