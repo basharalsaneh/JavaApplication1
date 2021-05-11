9d27f4136dc5529e32680d5600e05b57a55be389
maranv193 <marcus.fredriksson13@gmail.com>
2021-05-11 16:39:16 +0200

    Uppdaterat agentklassen

--- src/javaApplication1/Agent.java
+++ src/javaApplication1/Agent.java
@@ -1,4 +1,4 @@
-package TestDB123;
+package javaApplication1;
 
 import java.util.Date;
 import oru.inf.InfDB;
@@ -12,7 +12,7 @@ private int agent_ID;
 private String namn;
 private String Telefon;
 private String losenord;
-private boolean Administrator;
+private boolean administrator;
 private Date anstallningsdatum;
 
 public Agent(String namn, String Telefon, String losenord){
@@ -20,8 +20,12 @@ agent_ID = new Random().nextInt(10);
 this.namn = namn;
 this.Telefon = Telefon;
 this.losenord = losenord;
-Administrator = false;
+administrator = false;
 anstallningsdatum = new Date();
+}
+
+public int getAgentID(){
+return agent_ID;
 }
 
 public String getNamn(){
@@ -36,19 +40,32 @@ public Date getAnstallningsdatum(){
 return anstallningsdatum;
 }
 
-public int getAgentID(){
-return agent_ID;
-}
-
 public void bytLosenord(String nyttLosenord){
-    if(nyttLosenord.equals(losenord)){
-    System.out.println("Fel");
-    }
-    else{
-System.out.println("Du har bytt losenord till: " + nyttLosenord);
-System.out.println("Från: " + losenord);
-losenord = nyttLosenord;
-}}
-
-
+nyttLosenord = losenord;
 }
+
+public boolean getAdministrator(){
+return administrator;
+}
+
+public boolean becomeAdministrator(){
+administrator = true;
+return administrator;
+}
+
+public String getLosenord(){
+return losenord;
+}
+}
+
+ //*   public void listaAgenter(){
+      
+           ///for(int i = 0 ; i < agentLista.size() ; i++){
+              //  String agent = agentLista.get(i).getNamn();
+          
+              //  System.out.println(agent);
+              //  System.out.println("hej" + agent);
+            
+   // }
+  /// }  private ArrayList<Agent> agentLista = new ArrayList<>(); i konstruktorn (?)
+// agentLista.add (?)..
