f7c6409038041bbe485ba8a20f34ea222be2c968
Kavod1 <kristoffer.nilsson@tnk.se>
2021-05-12 11:27:26 +0200

    Försök att instansera inlogg på JavaApplication1

--- src/javaApplication1/JavaApplication1.java
+++ src/javaApplication1/JavaApplication1.java
@@ -10,21 +10,32 @@ public class JavaApplication1 {
     
 
     private static InfDB idb;
-
+    private static Inloggningssidan inlogg;
+    
+    
     public static void main(String[] args) throws InfException {
         
         System.out.println("Hej Bashar!");
         System.out.println("Hej Kristoffer!");
         System.out.println("Hej hej");
         System.out.println("dqwdqw");
-
-
+        
+        
+       
+      
         try {
-            idb = new InfDB("mibdb", "3307", "mibdba", "mibkey");
+            idb = new InfDB("mibdb", "3306", "mibdba", "mibkey");
 
         }   catch (InfException ex){
             Logger.getLogger(JavaApplication1.class.getName()).log(Level.SEVERE, null, ex);
         }
         
     }
+    
+    public JavaApplication1(){
+         inlogg = new Inloggningssidan();
+    }
+    
+    
+    
 }
