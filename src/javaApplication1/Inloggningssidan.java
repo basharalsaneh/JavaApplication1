66c18e3cf84bad80fadbb711b4dde0de0251ac75
basharalsaneh <83920082+basharalsaneh@users.noreply.github.com>
2021-05-18 13:19:21 +0200

    Update Inloggningssidan.java

--- src/javaApplication1/Inloggningssidan.java
+++ src/javaApplication1/Inloggningssidan.java
@@ -11,6 +11,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.sql.ResultSet;
 import java.sql.DriverManager;
+import javax.swing.JOptionPane;
 
 
 public class Inloggningssidan extends javax.swing.JFrame {
