07f956a3a889d6e8132a590978a6302ca19bc309
basharalsaneh <83920082+basharalsaneh@users.noreply.github.com>
2021-05-26 17:07:29 +0200

    Alien sida

--- src/javaApplication1/Alien.java
+++ src/javaApplication1/Alien.java
@@ -6,6 +6,7 @@
 package javaApplication1;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.JOptionPane;
@@ -28,43 +29,36 @@ public Alien(InfDB idb, inloggningValidering vemArInloggad) {
         initComponents();
         this.idb = idb;
         this.vemArInloggad = vemArInloggad;
-        
-    
+        jRub.setText("Välkommen " + vemArInloggad.getNamn() + "!");
+        alienInfoRutor();
     }
-      public void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
+
+  private void alienInfoRutor() {
         try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Alien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Alien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Alien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Alien.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
+            String fraga
+                    = "SELECT ag.Namn, ag.Telefon, o.Benamning\n"
+                    + "FROM alien al\n"
+                    + "JOIN plats p ON al.plats = p.plats_id\n"
+                    + "JOIN omrade o ON p.finns_i = o.omrades_id\n"
+                    + "JOIN omradeschef oc ON o.Omrades_ID = oc.Omrade\n"
+                    + "JOIN agent ag ON oc.Agent_ID = ag.Agent_ID\n"
+                    + "WHERE Alien_ID = " + "'" + vemArInloggad.getId() + "'";
+            
+            HashMap<String, String> result = idb.fetchRow(fraga);
+            String agentNamn = result.get("Namn");
+            String agentTel = result.get("Telefon");
+            String Omrade = result.get("Benamning");
 
-        /* Create and display the form */
-        java.awt.EventQueue.invokeLater(new Runnable() {
-            public void run() {
-                new Alien(idb, vemArInloggad).setVisible(true);
-            }
-        });
-    }
+            jChef.setText("Din chef: " + agentNamn);
+            jTel.setText("Din telefon: " + agentTel);
+            jOmrade.setText("Din område: " + Omrade);
+            
+        } catch (InfException ex) {
+            System.out.println("Databasfel" + ex.getMessage());
+        } catch (Exception ex) {
+            System.out.println("Random fel" + ex.getMessage());
+        }}
 
- 
-      
      /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -74,23 +68,16 @@ public Alien(InfDB idb, inloggningValidering vemArInloggad) {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        txtOmradeChef = new javax.swing.JTextField();
+        jChef = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
         jButton1 = new javax.swing.JButton();
-        buttonVisaOmradeChef = new javax.swing.JButton();
         btnLoggaUt = new javax.swing.JButton();
+        jTel = new javax.swing.JLabel();
+        jOmrade = new javax.swing.JLabel();
+        jRub = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setText("Din områdeschef:");
-
-        txtOmradeChef.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                txtOmradeChefActionPerformed(evt);
-            }
-        });
 
         jLabel3.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
 
@@ -104,13 +91,6 @@ public Alien(InfDB idb, inloggningValidering vemArInloggad) {
             }
         });
 
-        buttonVisaOmradeChef.setText("Visa");
-        buttonVisaOmradeChef.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                buttonVisaOmradeChefActionPerformed(evt);
-            }
-        });
-
         btnLoggaUt.setText("Logga ut");
         btnLoggaUt.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -118,83 +98,69 @@ public Alien(InfDB idb, inloggningValidering vemArInloggad) {
             }
         });
 
+        jRub.setFont(new java.awt.Font("Lucida Grande", 1, 18)); // NOI18N
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(buttonVisaOmradeChef, javax.swing.GroupLayout.PREFERRED_SIZE, 96, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(98, 98, 98)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addComponent(jLabel4)
-                                .addComponent(jLabel3)))
-                        .addGroup(layout.createSequentialGroup()
-                            .addGap(60, 60, 60)
-                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(btnLoggaUt)
-                                    .addGap(59, 59, 59)
-                                    .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE))
-                                .addGroup(layout.createSequentialGroup()
-                                    .addComponent(jLabel1)
-                                    .addGap(18, 18, 18)
-                                    .addComponent(txtOmradeChef, javax.swing.GroupLayout.PREFERRED_SIZE, 165, javax.swing.GroupLayout.PREFERRED_SIZE))))))
-                .addContainerGap(73, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(48, 48, 48)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addGroup(layout.createSequentialGroup()
+                                .addGap(12, 12, 12)
+                                .addComponent(btnLoggaUt)
+                                .addGap(59, 59, 59)
+                                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 133, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                                .addComponent(jOmrade, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jChef, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jTel, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(javax.swing.GroupLayout.Alignment.LEADING, layout.createSequentialGroup()
+                                    .addGap(50, 50, 50)
+                                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(jLabel4)
+                                        .addComponent(jLabel3))))))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(36, 36, 36)
+                        .addComponent(jRub, javax.swing.GroupLayout.PREFERRED_SIZE, 284, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addContainerGap(76, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(33, 33, 33)
-                .addComponent(jLabel3)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGap(17, 17, 17)
                 .addComponent(jLabel4)
-                .addGap(69, 69, 69)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jLabel1)
-                    .addComponent(txtOmradeChef, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(buttonVisaOmradeChef)
-                .addGap(37, 37, 37)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(btnLoggaUt))
-                .addContainerGap(49, Short.MAX_VALUE))
+                .addComponent(jLabel3)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(18, 18, 18)
+                        .addComponent(jRub, javax.swing.GroupLayout.PREFERRED_SIZE, 35, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(18, 18, 18)
+                        .addComponent(jChef)
+                        .addGap(18, 18, 18)
+                        .addComponent(jTel)
+                        .addGap(18, 18, 18)
+                        .addComponent(jOmrade)
+                        .addContainerGap(166, Short.MAX_VALUE))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                            .addComponent(jButton1)
+                            .addComponent(btnLoggaUt))
+                        .addGap(66, 66, 66))))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void txtOmradeChefActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtOmradeChefActionPerformed
-//    try {
-//        // TODO add your handling code here:
-//        
-//        int ID = vemArInloggad.getId();
-//        String fraga = "select a.Namn, p.Benamning, Alien_ID from alien\n" +
-//                "    join Plats p on p.Plats_ID=Alien_ID\n" +
-//                "join omrade om on om.Omrades_ID=p.Plats_ID\n" +
-//                "join omradeschef omc on omc.Omrade = om.Omrades_ID\n" +
-//                "join agent a on a.Agent_ID=omc.Agent_ID where alien_ID =" + ID + ";";
-//        ArrayList<String> minOmradeschef = idb.fetchColumn(fraga);
-//        JOptionPane.showMessageDialog(this, minOmradeschef);
-//    } catch (InfException ex) {
-//        Logger.getLogger(Alien.class.getName()).log(Level.SEVERE, null, ex);
-//    }
-        
-    }//GEN-LAST:event_txtOmradeChefActionPerformed
-
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         this.dispose();
               new BytaLosenordAlien(idb, vemArInloggad).setVisible(true);
     }//GEN-LAST:event_jButton1ActionPerformed
-
-    private void buttonVisaOmradeChefActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_buttonVisaOmradeChefActionPerformed
-        // TODO add your handling code here:
-        
-        // Här vill vi visa områdeschef i txt: txtOmradeChef.setText();
-    }//GEN-LAST:event_buttonVisaOmradeChefActionPerformed
 
     private void btnLoggaUtActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnLoggaUtActionPerformed
         // TODO add your handling code here:
@@ -205,11 +171,12 @@ public Alien(InfDB idb, inloggningValidering vemArInloggad) {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnLoggaUt;
-    private javax.swing.JButton buttonVisaOmradeChef;
     private javax.swing.JButton jButton1;
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jChef;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
-    private javax.swing.JTextField txtOmradeChef;
+    private javax.swing.JLabel jOmrade;
+    private javax.swing.JLabel jRub;
+    private javax.swing.JLabel jTel;
     // End of variables declaration//GEN-END:variables
 }
